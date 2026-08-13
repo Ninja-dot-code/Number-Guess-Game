f78b0c73d5403cbfb0e36256b374e63737d26aaa
Ninja-dot-code <nazeemakbar018@gmail.com>
2026-08-13 10:02:37 -0700

    This is first commit of guess game

---
+++ logic.js
@@ -0,0 +1,10 @@
+const ramdom = ((Math.random()*100) +1).toFixed()
+const button = document.querySelector('.subtn')
+const Field = document.querySelector('.guessfield')
+const Guesses= document.querySelector('.guesses')
+const remaining = document.querySelector('.lastresult')
+const loworhi = document.querySelector('.loworHi')
+const startover = document.querySelector('.parse-result')
+const p = document.createElement('p')
+
+
