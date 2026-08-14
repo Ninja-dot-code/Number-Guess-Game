500400ceca7054de00d003d3369d63cc0724940f
Ninja-dot-code <nazeemakbar018@gmail.com>
2026-08-14 00:11:16 -0700

    changing in javascript

--- logic.js
+++ logic.js
@@ -5,7 +5,33 @@ const Guesses= document.querySelector('.guesses')
 const remaining = document.querySelector('.lastresult')
 const loworhi = document.querySelector('.loworHi')
 const startover = document.querySelector('.parse-result')
-const p = document.createElement('p')
+const p = document.createElement('p');
+let guess = [];
+let numGuess = 1;
+let Gameplay = true;
+if(Gameplay){
+    
+}
 
+function validateguess(guess){
+
+}
+function checkguess(guess){
+
+}
+function displayguess(message){
+
+}
+function newgame(){
+    
+}
+
+function Endgame(){
+    
+}
+
+function newgame(){
+
+}
 
 
